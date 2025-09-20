4078d313c790fd6a60c225a9946b86b264832efa
Botond Vastag <vastag.botond@gmail.com>
2025-09-20 22:31:54 +0200

    using leader q for qa

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,4 +3,5 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>q', '<cmd>qa<CR>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
